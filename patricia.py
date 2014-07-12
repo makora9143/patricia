4e2faef44c894a10672f717b902cb5ae981a0f58
makora <makora@hts-MacBook-Pro.local>
2014-07-12 23:08:09 +0900

    modify patricia.py

--- patricia.py
+++ patricia.py
@@ -3,5 +3,18 @@
 
 """PATRICIA (Practical Algorithm To Retrieve Information Coded in Alphanumeric)"""
 
+from node import PNode
+
+class Patricia(object):
+    """Patrica class"""
+    def __init__(self):
+        self.root_node = PNode(None)
+        self.leaf_key = [None]
+
+    def search_nodes(self, search_key):
+        pass
+
+    def append_nodes(self, new_key):
+        pass
 
 # End of Line
